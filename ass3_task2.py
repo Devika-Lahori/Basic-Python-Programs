390987cff6940600ea07e6a15591d454b0412059
Devika-Lahori <devikalahori@gmail.com>
2025-06-27 16:33:37 +0530

    Add files via upload

---
+++ ass3_task2.py
@@ -0,0 +1,10 @@
+import math
+num = int(input("Enter a number: "))
+
+root = math.sqrt(num)
+log = math.log(num)
+sin = math.sin(num)
+
+print(f"Square Root: {root}")
+print(f"Logarithm: {log}")
+print(f"Sine: {sin}")
